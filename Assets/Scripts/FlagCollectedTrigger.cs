375481e5f98bad2a66980fb914381fdc87527b41
yonbav <yonbav@gmail.com>
2019-02-08 12:25:19 +0200

    Basic gameplay

---
+++ Assets/Scripts/FlagCollectedTrigger.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class FlagCollectedTrigger : MonoBehaviour
+{
+    public static int userScore;
+    
+    public GameObject UserCollectedFlagTrigger;
+    //public GameObject OpponentCollectedFlagTrigger;
+
+    public GameObject Gate;
+    public GameObject UserScore;
+    //public GameObject OpponentScore;
+
+    void Start()
+    {
+        UserCollectedFlagTrigger.SetActive(true);
+    }
+
+    void OnTriggerEnter()
+    {
+        // Increasing user score when triggers
+        userScore++;
+        
+        // Changing the user score on the score board
+        UserScore.GetComponent<Text>().text = userScore.ToString();
+
+        // Moving the 
+        MoveGate();
+    }
+
+    void MoveGate()
+    {
+        var position = Gate.transform.localPosition;
+        position.z += 10;
+        var rotation = Gate.transform.localRotation;
+        Gate.transform.SetPositionAndRotation(position, rotation); 
+    }
+}
---
+++ Assets/Scripts/TimeScreenManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeScreenManager : MonoBehaviour
+{
+    public TimeSpan TotalTime;
+
+    public static int playerLapsCount;
+    public static int OpponentLapsCount;
+    
+    public GameObject TimeValueLabel;
+
+
+    void Start()    
+    {
+        TotalTime = new TimeSpan(0);
+    }
+
+    void Update()
+    {
+        TotalTime = TotalTime.Add(TimeSpan.FromSeconds(Time.deltaTime));
+        TimeValueLabel.GetComponent<Text>().text =
+            $"{TotalTime.Minutes:D2}:{TotalTime.Seconds:D2}:{TotalTime.Milliseconds:D2}";
+
+    }
+}
